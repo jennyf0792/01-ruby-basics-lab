f1657f639110196e1d10c3b273ff0dc6629e640d
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2016-10-13 15:40:58 +0000

    complete ruby basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(divisor, dividend)
+     divisor / dividend
 end
 
-def argue
-  
+def argue(phrase)
+  "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+    topping
 end
 
-def whisper
-    
+def whisper(lowercase)
+    lowercase.downcase
 end
